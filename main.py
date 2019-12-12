a6fc8705937a37696b5f82f8fa00dd7462dfef23
Corentin PAPE <corentin.pape@epita.fr>
2019-12-12 14:34:05 +0100

    Architecture changes and main loop

--- main.py
+++ main.py
@@ -6,44 +6,41 @@ __docformat__ = "reStructuredText"
 Final project
 2019-12
 @author: marine.meurillon
-Requirements : googletrans and python-twitter and their dependancies
+Requirements : googletrans, emoji and python-twitter and their dependancies
 install with pip (or pip3)
 
 If any bug occurs, it may be due to changes on Twitter and Google side, provided that APIs are constantly evolving.
 """
-# imports
-from googletrans import Translator
 
-# variables
-translator = Translator()
-languageTab = [("fr","français"), ("en","anglais"), ("de", "allemand"),
-            ("ja", "japonais"), ("it", "italien"), ("es", "espagnol"),("el", "Greek"), ("eo", "Esperanto"), ("af", "Afrikaans"), ("sw", "Swahili"), ("ca", "Catalan"), ("iw", "Hebrew"), ("sv", "Swedish"), ("cs", "Czech"), ("cy", "Welsh"), ("ar", "Arabic"), ("ur", "Urdu"), ("ga", "Irish"), ("eu", "Basque"), ("et", "Estonian"), ("az", "Azerbaijani"), ("id", "Indonesian"), ("ru", "Russian"), ("gl", "Galician"), ("nl", "Dutch"), ("pt", "Portuguese"), ("la", "Latin"), ("tr", "Turkish"), ("tl", "Filipino")]
+
 
 """lv : Latvian, lt : Lithuanian, th : Thai, vi : Vietnamese, gu : Gujarati, ro : Romanian, 
 is : Icelandic, pl : Polish, ta : Tamil, yi : Yiddish, be : Belarusian, fr : French, bg : Bulgarian, uk : Ukrainian,
  hr : Croatian, bn : Bengali, sl : Slovenian, ht : Haitian Creole, da : Danish, fa : Persian, hi : Hindi,
  fi : Finnish, hu : Hungarian, ja : Japanese, ka : Georgian, te : Telugu, zh-TW : Chinese Traditional, sq : Albanian, no : Norwegian, 
 ko : Korean, kn : Kannada, mk : Macedonian, zh-CN : Chinese Simplified, sk : Slovak, mt : Maltese, de : German, ms : Malay, sr : Serbian"""
+# imports
+import translate
+import twi
 
+def getUserLanguage(username):
+    tweets = twi.getFirstsTweets(username)
+    return translate.detectInputsLanguage(tweets)
 
-
-# hand-made function library
-def languageName(abrev):
-    for couple in languageTab:
-        if couple[0] == abrev:
-            return couple[1]
-    return abrev
-
-def detectInputsLanguage(sentences):
+def getFollowersLanguage(username):
+    followers = twi.getFollowers(username)
+    if not followers:
+        print(username + " n'a pas de followers. C'est d'une tristesse :(")
+        return ["tristitude"]
     L = []
-    for s in sentences:
-        d = translator.detect(s)
+    for u in followers:
+        lang = getUserLanguage(username)
+        length = len(L)
         exists = False
-        for i in range(len(L)):
-            if d[0] == L[i][0]:
-                # add confidence
+        for i in range(length):
+            if lang == L[i][0]:
                 exists = True
-                L[i] = (d[0], L[i][1] + d[1])
+                L[i] = (lang, L[i][1] + 1)
                 # insertSort
                 if i != 0:
                     if L[i][1] > L[i-1][1]:
@@ -51,8 +48,75 @@ def detectInputsLanguage(sentences):
                         L[i] = L[i - 1]
                         L[i - 1] = tmp
         if not exists:
-            L.append(d)
+            L.append(lang)
     # returns the most spoke language
-    return languageName(L[0])
+    length = len(L)
+    for i in range(length)
+        L[i] = (translate.languageName(L[i][0]), (100 * L[i][1])/length)
+    return L
 
+def getFollowingsLanguage(username):
+    followings = twi.getFollowings(username)
+    if not followings:
+        print(username + " ne suit personne. Il ne se laisse pas influencer si facilement.")
+        return ["leaderspeak"]
+    L = []
+    for u in followings:
+        lang = getUserLanguage(username)
+        length = len(L)
+        exists = False
+        for i in range(length):
+            if lang == L[i][0]:
+                exists = True
+                L[i] = (lang, L[i][1] + 1)
+                # insertSort
+                if i != 0:
+                    if L[i][1] > L[i-1][1]:
+                        tmp = L[i]
+                        L[i] = L[i - 1]
+                        L[i - 1] = tmp
+        if not exists:
+            L.append(lang)
+    # returns the most spoke language
+    length = len(L)
+    for i in range(length)
+        L[i] = (translate.languageName(L[i][0]), (100 * L[i][1])/length)
+    return L
+
+
+# main loop
+
+print("============================================\n")
+print("Bienvenue dans InternationTwitterAnalytics !")
+print("Ce programme réalisé par Marine Meurillon et son équipe permet de recueillir des statitiques sur les langues parlées par un utilisateur de Twitter et ses proches.")
+print("Entrer \"quitter\" pour quitter")
+print("\n============================================")
+entree = ""
+while entree != "quitter":
+    entree = raw_input("Veuillez entrer un nom d'utilisateur: ")
+    if entree == "" or entree = "quitter":
+        continue
+    if not twi.userExists(entree):
+        print("L'utilisateur "+entree+" n'existe pas !")
+        continue
+    print("Utilisateur "+entree+" trouvé!")
+    print("Tentative d'identification de sa langue...")
+    lang = getUserLanguage(entree)
+    print(entree + " parle "+lang+".\nAppuyez sur entrée pour continuer...")
+    raw_input("")
+    print("Identification des langues de sa communauté...")
+    followersLang = getFollowersLanguage(entree)
+    followingsLang = getFollowingsLanguage(entree)
+    print("Affichage des résultats...\n")
+    print("================================")
+    print("Langues parlées par ses followers :")
+    for fLang in followersLang:
+        print(fLang[0] + " à "+fLang[1] + "%")
+    raw_input("Appuyez sur entrée pour continuer...")
+    print("================================")
+    print("Langues parlées par ceux qu'il suit :")
+    for fLang in followingsLang:
+        print(fLang[0] + " à "+fLang[1] + "%")
+    raw_input("Appuyez sur entrée pour continuer...")
+print("Merci d'avoir utilisé notre outil !")
 
